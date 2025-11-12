f8f9ef56e26c0d75cc4c0c4be28c44661890f0fd
ebube-anita <oluebubechukwuanita.com>
2025-11-12 14:36:54 +0100

    I don tire

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Fix 404 errors on page reload for client-side routing
-    historyApiFallback: true,
-  },
-  preview: {
-    // Also apply to production preview
-    historyApiFallback: true,
+    // React Router fallback
+    fs: { strict: false }
   }
-})
+});
